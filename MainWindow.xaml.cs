1aeb8910553b15a288febf44d96d65235dd236ce
JacobRochford <67964470+JacobRochford@users.noreply.github.com>
2026-05-03 13:08:53 -0700

    restore the required using directive

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Media3D;
 using System.Windows.Threading;
 using Microsoft.Win32;
 using MyNotes.Helpers;
